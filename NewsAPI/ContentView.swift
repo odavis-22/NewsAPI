4e134dbf41bf414d28a9b666559b09cb3e5143d5
Owen Davis <owendavis1115@gmail.com>
2024-12-10 15:06:20 -0500

    added API call to newsapi.org

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var json = "Loading..."
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("GoodBye, world!")
+            Text(json)
+        }
+        .task {
+            let url = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2024-11-10&sortBy=publishedAt&apiKey=94388a4be929440581310b7d3f2caedb")!
+
+            let (data, _) = try! await URLSession.shared.data(from: url)
+           
+            json = String(decoding: data, as: UTF8.self)
         }
         .padding()
     }
